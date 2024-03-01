d298fa08337b22615849dbc878024b74c30eded7
marcosd59 <damian.marcospool@gmail.com>
2024-03-01 07:28:58 -0500

    add solution for challenge #00

---
+++ Roadmap de retos/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO.js
@@ -0,0 +1,27 @@
+/*
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+
+// https://retosdeprogramacion.com/roadmap/
+
+// Comentario de una línea
+/* Comentario de varias líneas */
+
+let variable = "Hola, mundo";
+const constante = 3.141516;
+
+let cadena = "Esto es una cadena de texto";
+let entero = 10;
+let booleano = true;
+let nulo = null;
+let indefinido = undefined;
+
+console.log(`¡Hola, JavaScript!`);
